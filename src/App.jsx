42dcfbe6f7ad4274f156b3374e254447d8ad9310
chojeongeun <juy0816@naver.com>
2024-05-20 14:52:40 +0900

    메인 페이지 컨테이너 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import styled from "styled-components";
 import "./App.css";
 import Nav from "./components/Nav";
 
 function App() {
   return (
-    <>
+    <Container>
       <Nav />
       App
-    </>
+    </Container>
   );
 }
+
+const Container = styled.main`
+  position: realative;
+  display: block;
+  top: 70px;
+  padding: 0 calc(3.5vw + 5px);
+`;
 
 export default App;
