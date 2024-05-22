4533284130d6d8e7cff80da00d744ecfca163f67
chojeongeun <juy0816@naver.com>
2024-05-22 11:52:57 +0900

    Movie 모달 UI 생성하기

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,9 +1,18 @@
 import { useCallback, useEffect, useState } from "react";
 import "./Row.css";
 import axios from "../api/axios";
+import MovieModal from "./MovieModal";
 
 const Row = ({ title, id, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const [movieSelected, setMovieSelected] = useState({});
+
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelected(movie);
+  };
 
   const fetchMovieData = useCallback(async () => {
     const response = await axios.get(fetchUrl);
@@ -35,6 +44,7 @@ const Row = ({ title, id, fetchUrl }) => {
               className="row__poster"
               src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
               alt={movie.name}
+              onClick={() => handleClick(movie)}
             />
           ))}
         </div>
@@ -49,6 +59,9 @@ const Row = ({ title, id, fetchUrl }) => {
           </span>
         </div>
       </div>
+      {modalOpen ? (
+        <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+      ) : null}
     </div>
   );
 };
